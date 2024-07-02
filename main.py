527e3ee0583180d236f1f6d6f50be00c2f710b4e
yakshin_se <semenaksin13@gmail.com>
2024-07-02 19:08:18 +0300

    holy decompose

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pprint
+import sys
 from DataSourceHandlers.FileDataSourceHandler import FileDataSourceHandler
 from DataSourceHandlers.DataSourceHandlerInterface import DataSourceHandlerInterface
+sys.path.append('zparser/')
+import Zzz
+
 
 
 def main() -> int:
     data_source_handler: DataSourceHandlerInterface = FileDataSourceHandler('test_data_source.txt', ',', '\r\n')
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(data_source_handler.get_info())
+    # pp.pprint(data_source_handler.get_info())
+    print(Zzz.hello_from_cpp())
     return 0
 
 
