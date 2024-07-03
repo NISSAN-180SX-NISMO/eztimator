f7e90a8c7d16e42447308e66672ce1ed79122256
yakshin_se <semenaksin13@gmail.com>
2024-07-03 15:06:50 +0300

    Add functionality for zparser
    but work in main thread

--- main.py
+++ main.py
@@ -2,16 +2,50 @@ import pprint
 import sys
 from DataSourceHandlers.FileDataSourceHandler import FileDataSourceHandler
 from DataSourceHandlers.DataSourceHandlerInterface import DataSourceHandlerInterface
+import time
+import keyboard
 
 import zparser
 
+Parser = zparser.zparser()
+
+
+def on_d_pressed():
+    print("Button 'd' pressed")
+    Parser.do_work(5)
+
+
+
+
+def do_something():
+    try:
+        while True:
+            start_time = time.time()
+
+            # Выполнение функции do_something
+            work_status = "done" if Parser.checkWorkIsDone() == zparser.WORK_STATUS.DONE else "not done"
+            work_result = Parser.getWorkResult()
+
+            print(f"work status: {work_status}; work result: {work_result}")
+
+            # Проверка нажатия клавиши "d"
+            if keyboard.is_pressed('d'):
+                on_d_pressed()
+
+            # Задержка до следующей итерации (раз в секунду)
+            elapsed_time = time.time() - start_time
+            time.sleep(max(1.0 - elapsed_time, 0))
+    except KeyboardInterrupt:
+        print("Program interrupted and exiting...")
+
 
 def main() -> int:
-
     print(zparser.hello_from_cpp())
 
     z = zparser.zparser()
     print(z.hello_from_zparser())
+    Parser.init()
+    do_something()
     return 0
 
 
--- zparser_lib_cpp/python_interface.cpp
+++ zparser_lib_cpp/python_interface.cpp
@@ -1,8 +1,6 @@
-//
-// Created by user on 02/07/2024.
-//
 #include <pybind11/pybind11.h>
 #include "src/zparser.h"
+
 namespace py = pybind11;
 
 PYBIND11_MODULE(zparser, m) {
@@ -10,5 +8,15 @@ PYBIND11_MODULE(zparser, m) {
 
     py::class_<zparser>(m, "zparser")
         .def(py::init<>())
-        .def("hello_from_zparser", &zparser::hello_from_zparser);
-};
+        .def("hello_from_zparser", &zparser::hello_from_zparser)
+        .def("init", &zparser::init)
+        .def("do_work", &zparser::do_work)
+        .def("checkWorkIsDone", &zparser::checkWorkIsDone)
+        .def("getWorkResult", &zparser::getWorkResult);
+
+    py::enum_<zparser::WORK_STATUS>(m, "WORK_STATUS")
+        .value("IN_PROCESS", zparser::WORK_STATUS::IN_PROCESS)
+        .value("DONE", zparser::WORK_STATUS::DONE)
+        .value("NOTHING_TO_DO", zparser::WORK_STATUS::NOTHING_TO_DO)
+        .export_values();
+}
